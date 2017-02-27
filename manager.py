552b8091956fb713e9d09ceacf38da6b611b1538
wuli <wuli4444@163.com>
2017-02-27 09:13:40 +0800

    update manager with reserved port

--- manager.py
+++ manager.py
@@ -1,16 +1,19 @@
 from main import app
-from flask_script import Manager,Shell
+from flask_script import Manager,Shell,Server
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
-manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+server = Server(host="0.0.0.0",port=80)
+manager = Manager(app)
 
 def make_shell_context():
     return dict(app=app, db=db, NowTable=NowTable, LifeTable=LifeTable, User = User)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("runserver",Server())
 
 if __name__ == "__main__":
 
